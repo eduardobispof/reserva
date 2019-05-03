fd0f1d2102cfb36d3618478e744eeb72603e01aa
eduardobispof <eduardobispof@gmail.com>
2019-05-03 15:25:33 -0300

    criando relacionamento nas model's

--- app/Equipamento.php
+++ app/Equipamento.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App;
+use App\Tipo;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Equipamento extends Model
 {
-    //
+    protected $table = 'equipamentos';
+
+    protected $fillable = ['nome'];
+
+    public function tipos(){
+        return $this->hasMany(Tipo::class);
+    }
 }
